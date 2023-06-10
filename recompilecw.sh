8be3066359b69c62cbf1cad79b2ae3678f71d851
Tayrone Santos <34479816+jtayrone@users.noreply.github.com>
2023-06-10 00:24:51 -0300

    Create recompilecw.sh

---
+++ recompilecw.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo -i -u chatwoot && echo "Comando 'sudo -i -u chatwoot' executado com sucesso!"
+cd chatwoot && echo "Comando 'cd chatwoot' executado com sucesso!"
+git checkout develop && git pull && echo "Comandos 'git checkout develop' e 'git pull' executados com sucesso!"
+bundle && echo "Comando 'bundle' executado com sucesso!"
+yarn && echo "Comando 'yarn' executado com sucesso!"
+rake assets:precompile RAILS_ENV=production && echo "Comando 'rake assets:precompile RAILS_ENV=production' executado com sucesso!"
+RAILS_ENV=production bundle exec rake db:migrate && echo "Comando 'RAILS_ENV=production bundle exec rake db:migrate' executado com sucesso!"
