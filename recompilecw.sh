0a5a001a3aa049791dc15d56545557b7b563bae6
Tayrone Santos <34479816+jtayrone@users.noreply.github.com>
2023-06-13 13:29:16 -0300

    Update recompilecw.sh
    Comando o git pull para o modo deleloper.

--- recompilecw.sh
+++ recompilecw.sh
@@ -2,7 +2,7 @@
 
 sudo -i -u chatwoot && echo "Comando 'sudo -i -u chatwoot' executado com sucesso!"
 cd chatwoot && echo "Comando 'cd chatwoot' executado com sucesso!"
-git checkout develop && git pull && echo "Comandos 'git checkout develop' e 'git pull' executados com sucesso!"
+git checkout develop && git pull && echo "Comandos 'git checkout modo develop' e 'git pull' executados com sucesso!"
 bundle && echo "Comando 'bundle' executado com sucesso!"
 yarn && echo "Comando 'yarn' executado com sucesso!"
 rake assets:precompile RAILS_ENV=production && echo "Comando 'rake assets:precompile RAILS_ENV=production' executado com sucesso!"
